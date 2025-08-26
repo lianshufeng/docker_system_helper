7fc4004579876ed05c660b7708314bb95e2cc5fe
LianShuFeng <251708339@qq.com>
2025-08-26 22:00:06 +0800

    Create removeOldImages.sh.sh

---
+++ cmd/removeOldImages.sh.sh
@@ -0,0 +1,17 @@
+curl --unix-socket /var/run/docker.sock http://localhost/images/json \
+| jq -r '.[].RepoTags[]' \
+| awk -F: '{print $1, $2}' \
+| sort -u | cut -d' ' -f1 \
+| while read repo; do
+    tags=$(curl --unix-socket /var/run/docker.sock http://localhost/images/json \
+      | jq -r ".[] | select(.RepoTags != null) | .RepoTags[] | select(startswith(\"$repo:\"))" \
+      | cut -d: -f2 | sort -V)
+    latest=$(echo "$tags" | tail -n 1)
+    for tag in $tags; do
+      if [ "$tag" != "$latest" ]; then
+        echo "删除 $repo:$tag"
+        curl -s -X DELETE --unix-socket /var/run/docker.sock \
+          http://localhost/images/$repo:$tag
+      fi
+    done
+done
